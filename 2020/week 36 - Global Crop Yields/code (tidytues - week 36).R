5bc083a46a2a1c4d6de6c765cc544afe6baa2b99
RoxanneMoore <roxanne.f.moore@gmail.com>
2020-09-01 14:05:36 +0200

    Final #tidytuesday visual: Banana pun

--- 2020/week 36 - Global Crop Yields/code (tidytues - week 36).R
+++ 2020/week 36 - Global Crop Yields/code (tidytues - week 36).R
@@ -4,6 +4,7 @@
 # Install from CRAN via: 
 #install.packages("tidyverse")
 #install.packages("tidytuesdayR")
+# install.packages("gghighlights")
 
 # This loads the readme and all the datasets for the week of interest
 
@@ -21,14 +22,13 @@ arable_land <- tuesdata$arable_land
 # Load packages
 
 library(tidyverse)
+library(gghighlight)
 
 # View data
 
 glimpse(key_crop_yields)
 
 # Cleaning script
-
-library(tidyverse)
 
 long_crops <- key_crop_yields %>% 
         pivot_longer(cols = 4:last_col(),
@@ -52,7 +52,7 @@ ggplot(subset(long_crops, code == "USA"),
 usa_bananas <- long_crops %>%
         filter(crop == "Bananas", code == "USA") 
 
-ggplot(usa_bananas, aes(x = year, y = crop_production))                         + geom_line()                                                                + geom_text(aes(label = year))
+ggplot(usa_bananas, aes(x = year, y = crop_production))                         + geom_line()                                                                +  + geom_text(aes(label = year))
 
 # WHY? Difficultly subsetting based on two conditions in ggplot, so filtered at the dataset level
 # + geom_text(aes(label = year)) - enabled identification of 1983 and 2016 as banana production drop years
@@ -87,7 +87,7 @@ global_bananas <- long_crops %>%
                 na.omit(crop) %>% # drop countries without banana production
                 summarise(Mean1983 = mean(crop_production, na.rm = TRUE))
         
-         # mini answer - Average global banana crop production for 1982 was 13            .9. Whereas global banana crop production for 1983 was 13.5. Yes,             there was a global banana crop shortfall in 1983.
+         # mini answer - Average global banana crop production for 1982 was 13            .9. Whereas global banana crop production for 1983 was 13.5. Yes,              there was a global banana crop shortfall in 1983.
         
         global_banana_avg <- long_crops %>%
                 filter(crop == "Bananas") %>%
@@ -95,10 +95,40 @@ global_bananas <- long_crops %>%
                 group_by(year) %>%
                 summarize(global_avg = mean(crop_production)) # How do I convert "global_avg" as an entity and keep the numeric values as crop_production?
                 
-                
+      # mini question - How does USA banana production compare to the global average?
+        global_banana_avg <- long_crops %>%
+                filter(crop == "Bananas") %>%
+                na.omit(crop) %>%
+                group_by(year) %>%
+                # Create global average of Banana production per year
+                summarize(crop_production = mean(crop_production)) %>% 
+                # Add identifying variables for global average
+                mutate(entity = "Global average",
+                       code = "AVG",
+                       crop = "Bananas") %>%
+                # Reorder variables to match long_crop data frame
+                select(entity, code, year, crop, crop_production)
         
-        ggplot(global_banana_avg, aes(x = year, y = global_avg))                        + geom_line()                                                    
+        long_crop_avg <- long_crops %>%
+                bind_rows(global_banana_avg, long_crops) %>%
+                # %>% filter(entity == "Global average") - Test that row binding worked
+                filter(crop == "Bananas") %>%
+                na.omit(crop) %>%
+                filter(code == "USA" | code == "AVG")
+        
+       # Comparison of USA banana production vs. global average 
+        ggplot(long_crop_avg, aes(x = year, y = crop_production)) +                       geom_line(aes(group = code, color = entity)) +
+           scale_color_manual(values=c('grey68', 'black')) +
+           labs(x = "Year",
+                 y = "Crop production (tonnes per hectare)",
+                 title = "1992-2011: the United States was 'ripe' for banana production",
+                 caption = "Data Source: Our World in Data") +
+            geom_rect(aes(xmin=1992, xmax=2011, ymin=-Inf, ymax=Inf),
+                  fill = "gold2",
+                  alpha= 0.005,
+                  inherit.aes = FALSE) + # Add yellow banana highlighting
+            theme(legend.position="bottom", legend.title = element_blank())
+            # geom_text(aes(label = year)) # identify years
         
 ggplot(global_bananas, aes(x = year, y = crop_production))                      + geom_line(aes(group = code))                                                + facet_wrap(~code)
-
 
