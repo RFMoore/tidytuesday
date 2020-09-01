13676965b3221cf7fd9a96a33d9bcd20319c544c
Roxanne Moore <Roxanne.f.moore@gmail.com>
2020-09-01 14:10:13 +0200

    Update code (tidytues - week 36).R

--- 2020/week 36 - Global Crop Yields/code (tidytues - week 36).R
+++ 2020/week 36 - Global Crop Yields/code (tidytues - week 36).R
@@ -37,7 +37,7 @@ long_crops <- key_crop_yields %>%
         mutate(crop = str_remove_all(crop, " \\(tonnes per hectare\\)")) %>% 
         set_names(nm = names(.) %>% tolower())
 
-# EXPLAINATION - Gathers all crops  and tonnes per hectare into one column.  Previously unable to compare across crops due to seperate columns.
+# EXPLAINATION - Gathers all crops  and tonnes per hectare into one column. Previously unable to compare across crops due to seperate columns.
 
 long_crops
 
@@ -52,16 +52,16 @@ ggplot(subset(long_crops, code == "USA"),
 usa_bananas <- long_crops %>%
         filter(crop == "Bananas", code == "USA") 
 
-ggplot(usa_bananas, aes(x = year, y = crop_production))                         + geom_line()                                                                +  + geom_text(aes(label = year))
+ggplot(usa_bananas, aes(x = year, y = crop_production)) + geom_line() + geom_text(aes(label = year))
 
 # WHY? Difficultly subsetting based on two conditions in ggplot, so filtered at the dataset level
 # + geom_text(aes(label = year)) - enabled identification of 1983 and 2016 as banana production drop years
 
 # QUESTION 3: How do I annotate a specific section of a graph?
 
-banana_annotate <- ggplot(usa_bananas, aes(x = year, y = crop_production))         + geom_line()
+banana_annotate <- ggplot(usa_bananas, aes(x = year, y = crop_production)) + geom_line()
 
-banana_annotate + annotate("text", x = 1983, y = 5, label = "1983")             + annotate("text", x = 2016, y = 10.5, label = "2016")
+banana_annotate + annotate("text", x = 1983, y = 5, label = "1983") + annotate("text", x = 2016, y = 10.5, label = "2016")
 
 # Question 4: Was there a global banana production drop in 1983 and 2016, or just the USA?
 
@@ -74,9 +74,9 @@ global_bananas <- long_crops %>%
                 filter(crop == "Bananas") %>%
                 na.omit(crop) %>% # drop countries without banana production
                 distinct(code)
-        # mini answer - 136 countries... how do I visually production levels          for 136 countries?
+        # mini answer - 136 countries... how do I visually production levels for 136 countries?
         
-        # mini question - What is the average global crop production in 1982          compared to 1983?
+        # mini question - What is the average global crop production in 1982 compared to 1983?
         long_crops %>%
                 filter(crop == "Bananas", year == 1982) %>%
                 na.omit(crop) %>% # drop countries without banana production
@@ -87,7 +87,7 @@ global_bananas <- long_crops %>%
                 na.omit(crop) %>% # drop countries without banana production
                 summarise(Mean1983 = mean(crop_production, na.rm = TRUE))
         
-         # mini answer - Average global banana crop production for 1982 was 13            .9. Whereas global banana crop production for 1983 was 13.5. Yes,              there was a global banana crop shortfall in 1983.
+         # mini answer - Average global banana crop production for 1982 was 13.9. Whereas global banana crop production for 1983 was 13.5. Yes, there was a global banana crop shortfall in 1983.
         
         global_banana_avg <- long_crops %>%
                 filter(crop == "Bananas") %>%
@@ -117,7 +117,8 @@ global_bananas <- long_crops %>%
                 filter(code == "USA" | code == "AVG")
         
        # Comparison of USA banana production vs. global average 
-        ggplot(long_crop_avg, aes(x = year, y = crop_production)) +                       geom_line(aes(group = code, color = entity)) +
+        ggplot(long_crop_avg, aes(x = year, y = crop_production)) + 
+           geom_line(aes(group = code, color = entity)) +
            scale_color_manual(values=c('grey68', 'black')) +
            labs(x = "Year",
                  y = "Crop production (tonnes per hectare)",
@@ -130,5 +131,7 @@ global_bananas <- long_crops %>%
             theme(legend.position="bottom", legend.title = element_blank())
             # geom_text(aes(label = year)) # identify years
         
-ggplot(global_bananas, aes(x = year, y = crop_production))                      + geom_line(aes(group = code))                                                + facet_wrap(~code)
+ggplot(global_bananas, aes(x = year, y = crop_production)) + 
+        geom_line(aes(group = code)) + 
+        facet_wrap(~code)
 
