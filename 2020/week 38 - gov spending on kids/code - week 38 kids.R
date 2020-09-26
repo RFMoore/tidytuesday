181337ecb4f6b7dea47ec244f3691a8a2cfa94b2
RoxanneMoore <roxanne.f.moore@gmail.com>
2020-09-26 22:38:36 +0200

    max/min manipulations & boxplots

--- 2020/week 38 - gov spending on kids/code - week 38 kids.R
+++ 2020/week 38 - gov spending on kids/code - week 38 kids.R
@@ -35,6 +35,7 @@ kids %>%
         # RESULT: 1,140 USD spent per child on public health activities in 
         # Virginia in 2016.
 
+
 # Sub-component 2: What is the public health spending per child in VA across time?
 
 va_ph <- kids %>%
@@ -49,7 +50,8 @@ p + ggtitle("Public Health spending per child in Virginia 1997-2016") +
         
         # RESULT: Positive growth in public health spending for children 
         # adjusted for inflation. Double peaked, with an intermediate peak 2005
-        # -2006 and highest perak in 2016.
+        # -2006 and highest peak in 2016.
+
 
 # Sub-component 3: BONUS - All states across time
 ph <- kids %>%
@@ -65,3 +67,36 @@ p + ggtitle("Public Health spending per child 1997-2016",
             subtitle = "Adjusted for inflation (2016)") + 
         xlab("Year") + 
         ylab("Inflation-adjusted spending per child (in thousands)") 
+
+        # Further questions:
+                # What states have the greatest change in spending (all time)?
+                # What states are spending the most on PH (all time)?
+                # What states are spending the most on PH (current)?
+
+        # What states have the greatest change in spending (all time)?
+
+
+p <- ggplot(ph, aes(x = fct_reorder(state, inf_adj_perchild, FUN = median, .desc = TRUE), y = inf_adj_perchild)) + 
+        geom_boxplot() 
+
+p + ggtitle("Public Health spending per child 1997-2016",
+            subtitle = "Adjusted for inflation (2016)") + 
+        xlab("Year") + 
+        ylab("Inflation-adjusted spending per child (in thousands)") 
+
+
+# CODE THAT I LEARNED SOMETHING FROM BUT TURNED OUT WRONG
+
+# What states have the greatest change in spending (all time)?
+
+#ph_mm <- kids %>%
+        #filter(variable == "pubhealth") %>%
+        #group_by(state) %>%
+        #mutate(max_iap = max(inf_adj_perchild), # new column min per state
+         #      min_iap = min(inf_adj_perchild)) # new column max per state
+
+        # LEARNING: 
+        # mutate rather than summarize to keep original data table
+        # summarize(var == on max(var) converts to element-wise comparison 
+                # giving "TRUE"/"FALSE" for all values
+        # As long as there is a "," or a "+" can line break but maintain code
