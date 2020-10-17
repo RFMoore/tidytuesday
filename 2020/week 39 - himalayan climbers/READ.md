1fcd1b8faa1920f4adc8d34a6f96205d6f3f9139
RoxanneMoore <roxanne.f.moore@gmail.com>
2020-10-17 15:49:15 +0200

    Final review, time cut-off

--- 2020/week 39 - himalayan climbers/READ.md
+++ 2020/week 39 - himalayan climbers/READ.md
@@ -1,6 +1,101 @@
 ### Lessons (semi-) Learned:
 
 - skimr package produces summary statistics with results grouped by variable type
+- Various visualization packages:
+  - geom_points(): Vertical dot plot. Transparency or color enables density
+  - geom_beeswarm(): Vertical dot plot, with additional values aligned horizontally
+  - geom_quasirandom(): Beeswarm jittered to create a dot-driven violin plot
+  - geom_violin(): Density/shape of distribution
+- Color by count:
+  - Compute color or size by data values (..n..)
+  - Create additional variable that serves as "count" (group_by > summarize > count)
+- Binning: mutate(round())
+
 
 ### To learn:
+The "Expeditions" table provided number of persons and highest elevation 
+reached by various expeditions. If the "highest elevations" were binned, then 
+the number of persons ("members" and "hired_staff") can be summed by each 
+elevation bin. In order to calculate the proportional injury need the total 
+number of all persons who reached that elevation or above (passed through in 
+both summit and descent). Relatively few hikes start from 0m. If trying to 
+calculate the denominator (all persons who have reached that elevation or above),
+need to ensure the minimum start elevation.
 
+
+### Impression:
+For the purposes of this analysis, the specific elevation of injury mattered less
+than the general trend. As the purpose was to identify data-driven rules of thumb
+to guide hiking injury prevention. For example when reaching 6,000m become 
+particularly vigilent about "x", when reaching 7,000m look out for "y". This led 
+to two challenges in visualization/interpretation:
+
+##### 1. Number of missing values. 
+
+The following table shows the count and proportion of missing values by injury type,
+following the same order as in the final plot...
+
+| Injury Type | Count Missing | Proportion Missing |
+| --- | --- | --- |
+| AMS | 189 | 45.5% |
+| Avalanche| 9 | 7.9% |
+| Crevasse | 8 | 32% | 
+| Disappearance | 8 | 100% |
+| Exhaustion | 4 | 57.1% |
+| Exposure / frostbite | 193 | 32.2% |
+| Fall | 33 | 28.2% |
+| Falling rock / ice | 26 | 29.5% |
+| Icefall collapse | 3 | 42.9% |
+| Illness (non-AMS) | 180 | 70.0% |
+| Other | 50 | 66.7% |
+
+...This highlights a few issues. First, all data related to "Disappearance"
+disappears from the final plot as by the nature of the event location/elevation 
+is unknown. Second, many of the injuries are small events where even relatively
+limited missing values (e.g. 4 elevations values missing for "Exhaustion") have 
+a significant impact upon representation (e.g. over half (57%) of "Exhaustion"
+injuries are missing).
+
+When re-ordering the table by highest missing counts (as to group the small events)... 
+
+| Injury Type | Count Missing | Proportion Missing |
+| --- | --- | --- |
+| Exposure / frostbite | 193 | 32.2% |
+| AMS | 189 | 45.5% |
+| Illness (non-AMS) | 180 | 70.0% |
+| Other | 50 | 66.7% |
+| Fall | 33 | 28.2% |
+| Falling rock / ice | 26 | 29.5% |
+| Avalanche| 9 | 7.9% |
+| Crevasse | 8 | 32% | 
+| Disappearance | 8 | 100% |
+| Exhaustion | 4 | 57.1% |
+| Icefall collapse | 3 | 42.9% |
+
+... over half of the injury types had over 20 missing values with a proportional 
+impact of 28-70% of total injury type values being missing. "Exposure / frostbite" 
+is the most distintively graphed injury type within the final plot, yet still 
+has 32% missingness. "AMS" although relatively indistinct in the current graph, 
+may have told a unique story if not for nearly half (45.5%) of its elevation 
+values being missing. "Illness (non-AMS)" may appear slightly more prevelant at 
+lower levels, but with 70% of the values being ignored that interpretation is
+relatively meaningless.
+
+
+##### 2. Counts relative to what?
+
+The raw injury counts tell me nothing about proportion of occurance. For example,
+over 200 people had "Exposure / frostbite" at 8,000m. However from the existing 
+graph I have no idea whether that was all people who went up to 8,000m or only a 
+small number. If the original intent was to establish general "rules of thumb", 
+then the proportion of occurance is super important. Steps to make this happen 
+under "To learn".
+
+
+##### Honorable mention: How to represent density
+
+I tried eight different approaches (times two - unbinned & binned) to tackle my
+desire for vertical heatmaps showing injury. This resulted in two different 
+groups of visuals - those showing injury density by color and those showing 
+injury density by size. The final plot tries to integrate both (color and size), 
+but generally feeling meh.
--- 2020/week 39 - himalayan climbers/code - himalayan climbers.R
+++ 2020/week 39 - himalayan climbers/code - himalayan climbers.R
@@ -188,14 +188,14 @@ write_csv(peaks, "./himalayan-expeditions/peaks.csv")
 
 # PREVIEW DATA
 glimpse(members)
-skim(members) # summary statistics grouped byvariable type
+skim(members) # summary statistics grouped by variable type
 
 # RESEARCH QUESTION:
 # What injuries are associated with climbing Everest?
         #SUB-COMPONENTS:
-        # How many people have been injuried while climbing Everest?
+        # How many people have been injured while climbing Everest?
         # What type of injuries occurred?
-        # Where are injuries occuring (elevation)?
+        # Where are injuries occurring (elevation)?
         # Has the type of injury changed over time?
         # Who has been injured?
         # BONUS: Are there characteristics of climbers associated with higher or
@@ -203,13 +203,13 @@ skim(members) # summary statistics grouped byvariable type
                 # acclimated to high altitudes – less likely to suffer from AMS?
 
 
-# SUB-COMPONENT 1: How many people have been injuried while climbing Everest?
+# SUB-COMPONENT 1: How many people have been injured while climbing Everest?
 
 members %>%
         filter(peak_name == "Everest" & injured == "TRUE") %>%
         count(injured)
 
-        # RESULT: 485 injuries while climbining Everest
+        # RESULT: 485 injuries while climbing Everest.
 
 
 # SUB-COMPONENT 2: What type of injuries occurred?
@@ -227,187 +227,176 @@ members %>%
 members %>%
         filter(peak_name == "Everest" & injured == "TRUE") 
 
-# All in one line, black transparency to show density
-ggplot(members, aes(x = injury_type, y = injury_height_metres)) + 
-        geom_point(alpha = 1/10) # transparency
+# Plot 1 - un-binned attempts
+        # All in one line, black transparency to show density
+        ggplot(members, aes(x = injury_type, y = injury_height_metres)) + 
+                geom_point(alpha = 1/10) # transparency
 
-# Beeswarm - horizontal lining to show density
-ggplot(members, aes(x = injury_type, y = injury_height_metres)) + 
-        geom_beeswarm()
-
-# Jitter the data points (lose specificty elevation) to show density
-ggplot(members, aes(x = injury_type, y = injury_height_metres)) + 
-        geom_quasirandom() 
-
-# Violin plot - width to show density
-ggplot(members, aes(x = injury_type, y = injury_height_metres)) + 
-        geom_violin()
-
-# dot color/size density - inversed colors
-ggplot(members, aes(x = injury_type, y = injury_height_metres)) + 
-        geom_count(aes(color = ..n.., size = ..n..), # ..n.. = computed var
-                   alpha = .7) + # transparency
-        guides(color = 'legend') + #display color as legend
-        scale_size_area(max_size = 10)
-
-# Color based on count variable
-count <- members %>%
-        filter(peak_name == "Everest" & injured == "TRUE") %>%
-        group_by(injury_type, injury_height_metres) %>%
-        summarise(count = n()) 
-
-        ggplot(count, aes(injury_type, injury_height_metres, color=count)) +
-        geom_point(size = 5, 
-                   alpha = .6,
-                   method = "lm", 
-                   show.legend = FALSE) +
-        scale_color_viridis(direction = -1)
-
-# Color based on count, jittered
-ggplot(count, aes(injury_type, injury_height_metres, color=count)) +
-        geom_quasirandom(size = 2, cex = 2) + 
-        scale_color_viridis(direction = -1)
+        # Beeswarm - horizontal lining to show density
+        ggplot(members, aes(x = injury_type, y = injury_height_metres)) + 
+                geom_beeswarm()
         
+        # Jitter the data points (lose specificity elevation) to show density
+        ggplot(members, aes(x = injury_type, y = injury_height_metres)) + 
+                geom_quasirandom() 
 
-count_yr <- members %>%
-        filter(peak_name == "Everest" & injured == "TRUE") %>%
-        group_by(injury_type, injury_height_metres, year) %>%
-        summarise(count = n()) 
+        # Violin plot - width to show density
+        ggplot(members, aes(x = injury_type, y = injury_height_metres)) + 
+                geom_violin()
+        
+        # dot color/size density - inverse colors
+        ggplot(members, aes(x = injury_type, y = injury_height_metres)) + 
+                geom_count(aes(color = ..n.., size = ..n..), # ..n.. = computed var
+                           alpha = .7) + # transparency
+                guides(color = 'legend') + #display color as legend
+                scale_size_area(max_size = 10)
+        
+        # Color based on count variable
+        count <- members %>%
+                filter(peak_name == "Everest" & injured == "TRUE") %>%
+                group_by(injury_type, injury_height_metres) %>%
+                summarise(count = n()) 
+        
+                ggplot(count, aes(injury_type, injury_height_metres, color=count)) +
+                geom_point(size = 5, 
+                           alpha = .6,
+                           method = "lm", 
+                           show.legend = FALSE) +
+                scale_color_viridis(direction = -1)
+        
+        # Color based on count, jittered
+        ggplot(count, aes(injury_type, injury_height_metres, color=count)) +
+                geom_quasirandom(size = 2, cex = 2) + 
+                scale_color_viridis(direction = -1)
+                
 
-# Injuries by year (attempt to animate)
-ggplot(count_yr, aes(injury_type, injury_height_metres, size = count, frame = year)) +
-        geom_point() +
-        geom_smooth(aes(group = year), 
-                    method = "lm", 
-                    show.legend = FALSE) +
-        facet_wrap(~injury_type, scales = "free") +
-        #scale_x_log10()  # convert to log scale
-
-gganimate(g, interval=0.2)
-
-# Aggregate injury elevation from rounded to 10's place to rounded to 100's
-
-
-
-r_members <- members %>%
-        filter(peak_name == "Everest" & injured == "TRUE") %>%
-        mutate(ihm_100s = round(injury_height_metres, digits = -2))
-
-glimpse(r_members)
-
-# All in one line, black transparency to show density
-ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
-        geom_point(alpha = 1/10) # transparency
-
-# Beeswarm - horizontal lining to show density
-ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
-        geom_beeswarm()
-
-# Jitter the data points (lose specificty elevation) to show density
-ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
-        geom_quasirandom() 
-
-# Violin plot - width to show density
-ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
-        geom_violin()
-
-# dot color/size density - inversed colors
-ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
-        geom_count(aes(color = ..n.., size = ..n..), # ..n.. = computed var
-                   alpha = .7) + # transparency
-        guides(color = 'legend') + #display color as legend
-        scale_size_area(max_size = 10)
-
-# Color based on count variable
-count <- r_members %>%
-        filter(peak_name == "Everest" & injured == "TRUE") %>%
-        group_by(injury_type, ihm_100s) %>%
-        summarise(count = n()) 
-
-ggplot(count, aes(injury_type, ihm_100s, color=count)) +
-        geom_point(size = 5, 
-                   alpha = .6,
-                   method = "lm", 
-                   show.legend = FALSE) +
-        scale_color_viridis(direction = -1)
-
-# Color based on count, jittered
-ggplot(count, aes(injury_type, ihm_100s, color=count)) +
-        geom_quasirandom(size = 2, cex = 2) + 
-        scale_color_viridis(direction = -1)
-
-# Injuries by year (attempt to animate)
-count_yr <- r_members %>%
-        filter(peak_name == "Everest" & injured == "TRUE") %>%
-        group_by(injury_type, ihm_100s, year) %>%
-        summarise(count = n()) 
-
-
-ggplot(count_yr, aes(injury_type, ihm_100s, size = count, frame = year)) +
-        geom_point() +
-        geom_smooth(aes(group = year), 
-                    method = "lm", 
-                    show.legend = FALSE) +
-        facet_wrap(~injury_type, scales = "free") +
-        #scale_x_log10()  # convert to log scale
+        count_yr <- members %>%
+                filter(peak_name == "Everest" & injured == "TRUE") %>%
+                group_by(injury_type, injury_height_metres, year) %>%
+                summarise(count = n()) 
+        
+        # Injuries by year (attempt to animate)
+        ggplot(count_yr, aes(injury_type, injury_height_metres, size = count, frame = year)) +
+                geom_point() +
+                geom_smooth(aes(group = year), 
+                            method = "lm", 
+                            show.legend = FALSE) +
+                facet_wrap(~injury_type, scales = "free") +
+                #scale_x_log10()  # convert to log scale
         
         gganimate(g, interval=0.2)
 
+# Plot 2 - Binned
+        # Aggregate injury elevation from rounded to 10's place to rounded to 100's
+        r_members <- members %>%
+                filter(peak_name == "Everest" & injured == "TRUE") %>%
+                mutate(ihm_100s = round(injury_height_metres, digits = -2))
+        
+        glimpse(r_members)
+        
+        # All in one line, black transparency to show density
+        ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
+                geom_point(alpha = 1/10) # transparency
+        
+        # Beeswarm - horizontal lining to show density
+        ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
+                geom_beeswarm()
+        
+        # Jitter the data points (lose specificty elevation) to show density
+        ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
+                geom_quasirandom() 
+        
+        # Violin plot - width to show density
+        ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
+                geom_violin()
+        
+        # dot color/size density - inversed colors
+        ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
+                geom_count(aes(color = ..n.., size = ..n..), # ..n.. = computed var
+                           alpha = .7) + # transparency
+                guides(color = 'legend') + #display color as legend
+                scale_size_area(max_size = 10)
+        
+        # Color based on count variable
+        count <- r_members %>%
+                filter(peak_name == "Everest" & injured == "TRUE") %>%
+                group_by(injury_type, ihm_100s) %>%
+                summarise(count = n()) 
+        
+        ggplot(count, aes(injury_type, ihm_100s, color=count)) +
+                geom_point(size = 5, 
+                           alpha = .6,
+                           method = "lm", 
+                           show.legend = FALSE) +
+                scale_color_viridis(direction = -1)
+        
+        # Color based on count, jittered
+        ggplot(count, aes(injury_type, ihm_100s, color=count)) +
+                geom_quasirandom(size = 2, cex = 2) + 
+                scale_color_viridis(direction = -1)
+        
+        # Injuries by year (attempt to animate)
+        count_yr <- r_members %>%
+                filter(peak_name == "Everest" & injured == "TRUE") %>%
+                group_by(injury_type, ihm_100s, year) %>%
+                summarise(count = n()) 
 
-#MOVE FORWARD WITH VIOLIN AND DOT DENSITY
 
+        ggplot(count_yr, aes(injury_type, ihm_100s, size = count, frame = year)) +
+                geom_point() +
+                geom_smooth(aes(group = year), 
+                            method = "lm", 
+                            show.legend = FALSE) +
+                facet_wrap(~injury_type, scales = "free") +
+                #scale_x_log10()  # convert to log scale
+                
+                gganimate(g, interval=0.2)
+
+
+#MOVE FORWARD WITH BINNED DOT DENSITY
+
+# Data - Bins
 r_members <- members %>%
         filter(injured == "TRUE") %>%
         mutate(ihm_100s = round(injury_height_metres/250)*250)
 
-glimpse(r_members)
-
-# Violin plot - width to show density
-ggplot(count, aes(x = injury_type, y = ihm_100s)) + 
-        geom_violin()
-
-# dot color/size density - inversed colors
-p <- ggplot(r_members, aes(x = injury_type, y = ihm_100s)) + 
-        geom_count(aes(color = ..n.., size = ..n..), # ..n.. = computed var
-                   alpha = .7) + # transparency
-        guides(color = 'legend') + #display color as legend
-        scale_size_area(max_size = 10) 
-
-# Titles
-p + labs(title = "Injury in the Himalayas by elevation", 
-         x = "Injury type",
-         y = "Injury height (meters)",
-         #fill = "Number of injuries",
-         caption = "Source: The Himalayan Database") 
-
-# dot color/size density - inversed colors
-count <- r_members %>%
+        # SUB-ANALYSIS: Count NAs by injury type
+        nas <- members %>%
+                filter(injured == "TRUE") %>%
+                mutate(ihm_100000s = round(injury_height_metres/100000)*100000) %>%
+                # Bin "values" vs "NA"s
+                count(injury_type, ihm_100000s) %>%
+                group_by(injury_type) %>%
+                mutate(freq = n / sum(n))
+                         
+# Data - Counts of injury by bins
+c_data <- r_members %>%
         filter(injured == "TRUE") %>%
         group_by(injury_type, ihm_100s) %>%
         summarise(count = n()) %>%
         drop_na()
 
-p <- ggplot(count, aes(x = injury_type, y = ihm_100s, color = count, size = count)) + 
+# Plot
+p <- ggplot(c_data, aes(x = injury_type, y = ihm_100s, color = count, size = count)) +
         geom_point(alpha = .7) + # transparency
-        guides(color = 'legend') + #display color/size as legend
-        scale_size_area(max_size = 10) 
+        guides(color = 'legend') + #display color/size as legend 
+        scale_size_area(max_size = 15) +
+        scale_colour_gradient(low = "#fee0d2", high = "#de2d26") 
 
 # Titles
 p + labs(title = "Injury in the Himalayas by elevation", 
          x = "Injury type",
          y = "Injury height (meters)",
          #fill = "Number of injuries",
-         caption = "Source: The Himalayan Database") 
-
-# Color based on count variable
-count <- r_members %>%
-        filter(injured == "TRUE") %>%
-        group_by(injury_type, ihm_100s) %>%
-        summarise(count = n()) 
-
-ggplot(count, aes(injury_type, ihm_100s, color=count)) +
-        geom_point(size = 5, 
-                   alpha = .6,
-                   method = "lm", 
-                   ) + #show.legend = FALSE
-        scale_color_viridis(direction = -1)
+         caption = "Source: The Himalayan Database") +
+    scale_y_continuous(breaks = seq(0, 9000, by = 2000)) + 
+    scale_x_discrete(labels = c("AMS", 
+                                    "Avalanche",
+                                    "Crevasse",
+                                    "Exhaustion",
+                                    "Exposure\n/ frostbite", #line breaks in values
+                                    "Fall",
+                                    "Falling \nrock / ice",
+                                    "Icefall\ncollapse",
+                                    "Illness\n(non-AMS)",
+                                    "Other")) 
